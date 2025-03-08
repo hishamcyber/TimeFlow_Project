cb0ba44e755db446f609084cd006557dc61f4a9b
Hisham <hichambsnman@gmail.com>
2025-03-08 21:06:27 +0800

    init

--- app.py
+++ app.py
@@ -10,6 +10,21 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/employee_dashboard')
+def employee_dashboard():
+    return render_template('employee_dashboard.html')
+
+@app.route('/boss_dashboard')
+def boss_dashboard():
+    return render_template('boss_dashboard.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
